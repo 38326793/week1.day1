96ef525c2699c732199589ce862ddfe18177de52
38326793 <95647487+38326793@users.noreply.github.com>
2021-12-07 20:19:37 +0530

    Add files via upload

---
+++ Day1Assignment_OddNumbers.java
@@ -0,0 +1,17 @@
+package week1.day1.assignments;
+
+public class Day1Assignment_OddNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("Printing the odd numbers from 1 to 20");
+		System.out.println("***************************************");
+		
+		for(int i=1;i<=20;i++){
+			if(i % 2 == 1)
+			{
+					System.out.println(i);
+			}
+	}
+
+}
+}
